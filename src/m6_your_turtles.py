433576bb6fab942cd0b877fc3515221dad16de35
rifecj <rifecj@rose-hulman.edu>
2019-03-04 15:21:44 -0500

    done

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -32,8 +32,20 @@ import rosegraphics as rg
 window = rg.TurtleWindow()
 charlie=rg.SimpleTurtle('turtle')
 charlie.pen = rg.Pen('red', 5)
+charlie.speed=20
 for k in range(40):
-    charlie.forward(k)
+    charlie.forward(k*5)
     charlie.right(44)
     charlie.forward(k/2)
     charlie.right(44)
+randy=rg.SimpleTurtle('turtle')
+randy.pen = rg.Pen('blue', 5)
+randy.speed=20
+randy.right(90)
+for k in range(40):
+    randy.forward(k*5)
+    randy.right(44)
+    randy.forward(k/2)
+    randy.right(44)
+
+window.close_on_mouse_click()
--- src/m7_summary.py
+++ src/m7_summary.py
@@ -2,10 +2,10 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Chloe Rife.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -43,3 +43,15 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow()
+charlie=rg.SimpleTurtle('turtle')
+charlie.pen = rg.Pen('blue', 5)
+charlie.left(90)
+charlie.forward(200)
+charlie.pen_up()
+charlie.go_to(rg.Point(100, -40))
+charlie.pen_down()
+charlie.pen=rg.Pen('green',10)
+charlie.backward(150)
+window.close_on_mouse_click()
